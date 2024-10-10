eec8b5e9caf6e854fc5c27abc381f640163da59d
elvannl <97175534+elvannunal@users.noreply.github.com>
2024-10-10 21:13:13 +0300

    update task dto changed

--- TaskStream.EntityLayer/TaskStream.EntityLayer/Dtos/UpdateTask.cs
+++ TaskStream.EntityLayer/TaskStream.EntityLayer/Dtos/UpdateTask.cs
@@ -3,12 +3,14 @@ using TaskStream.EntityLayer.Entity;
 
 namespace TaskStream.EntityLayer.Dtos;
 
-public class UpdateTask: BaseEntity
+public class UpdateTask
 {
+
+    public Guid Id { get; set; }
     public string? UserTaskName { get; set; }
     public string? Description { get; set; }
     public TaskStatus? Status { get; set; }
     public Guid? AssigneeId { get; set; }
     public Guid? TeamId { get; set; }
-    public Team? Team { get; set; }
+  //  public Team? Team { get; set; }
 }
